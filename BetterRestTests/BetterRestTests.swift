1d5df3c2cd80269c1cb57b64b90366abefb1ed29
pranay-vohra <pranayvohra77@gmail.com>
2024-11-11 22:49:41 +0530

    Initial Commit

---
+++ BetterRestTests/BetterRestTests.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestTests.swift
+//  BetterRestTests
+//
+//  Created by pranay vohra on 11/11/24.
+//
+
+import Testing
+@testable import BetterRest
+
+struct BetterRestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
